1a72cc664bf0c8fb009a1d4bfccd82fed7286117
ElisaMarchete <elisamarchete@gmail.com>
2023-02-09 14:09:12 -0500

    data and demo added to js files

---
+++ jsdemo.js
@@ -0,0 +1,40 @@
+// IF STATEMENT 
+/* var topic = "HTML";
+
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+} */
+
+
+// ARRAYS
+/* var shapes = ["triangle", "square", "pentagon", "circle"]; 
+shapes[0]; to call an aray above
+console.log(shapes[0]); to call the array above
+var studentInfo = ["Lu", 54, true];  */
+
+// THE FOR LOOP
+// var shapes = ["triangle", "square", "pentagon", "circle"];
+// console.log(shapes[0]);
+// console.log(shapes[1]);
+// console.log(shapes[2]);
+// console.log(shapes[3]);
+
+// Instead of the code above create a for loop below!
+
+/* var x = 0; //starting point for the loop. To start at the beginning of the array (because the first index of an array is always 0)
+x < shapes.length; //condition. This will allow the loop to execute one time for each item in the array.
+x++ //The code x++ is a shortcut way of writing x+1. Now every time the code loops, x will increase by 1
+
+var shapes = ["triangle", "square", "pentagon", "circle"];
+for(var x = 0; x < shapes.length; x++) {
+ console.log(shapes[x]);
+}
+*/ 
